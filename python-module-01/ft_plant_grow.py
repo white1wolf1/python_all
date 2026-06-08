cdbe376217fe3f5cfe0ba648bf8f3d74878198d9
white1wolf1 <96636096+white1wolf1@users.noreply.github.com>
2026-06-09 00:46:57 +0300

    Implement Plant class with growth and aging methods

---
+++ python-module-01/ft_plant_grow.py
@@ -0,0 +1,37 @@
+class Plant:
+    def __init__(self, name: str, height: float, days: int) -> None:
+        self.name = name
+        self.height = height
+        self.days = days
+
+    def grow(self) -> None:
+        if self.name == "Rose":
+            self.height += 1
+        elif self.name == "Sunflower":
+            self.height = round(self.height + 0.4, 2)
+        elif self.name == "Cactus":
+            self.height += round(self.height + 0.2, 2)
+        else:
+            self.height += round(self.height + 1.1, 2)
+
+    def age(self) -> None:
+        print("=== Garden Plant Growth ===")
+        print(f"{self.name} : {self.height}cm, {self.days} days old")
+        i = 1
+        value = self.height
+        for i in range(1, 8):
+            self.grow()
+            self.days += 1
+            print(f"=== Day {i} ===")
+            print(f"{self.name} : {self.height}cm, {self.days} days old")
+        value = self.height - value
+        print(f"Growth this week: {value}cm")
+
+
+def main() -> None:
+    Plant1 = Plant("Cactus", 25, 30)
+    Plant1.age()
+
+
+if __name__ == "__main__":
+    main()
