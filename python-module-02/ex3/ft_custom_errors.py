a8b5684ea188d4cf5f445f8cda5d0defd23ca328
alex44 <sergeyevnasa@gmail.com>
2026-07-28 01:36:55 +0300

    kendime not

--- python-module-02/ex3/ft_custom_errors.py
+++ python-module-02/ex3/ft_custom_errors.py
@@ -1,8 +1,17 @@
 class GardenError(Exception):
-    pass
+   def  __init__(self,error_message = "hatanın bos olduğu hata mesajı"):
+       super().__init__(error_message)
 
-class PlantError(GardenError):
-    pass
 
-class WaterError(GardenError):
-    pass
+
+class Deneme:
+    def deneme():
+        try:
+            raise GardenError(" verilen mesaj") 
+        except GardenError as e:
+            print(e)
+
+
+    
+if __name__ == "__main__":
+    Deneme.deneme()
