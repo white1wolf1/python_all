eb14940df710c9949b84f07df083c64aa875b85f
Alexandra Soboleva <asobolev@k1m09s02.42istanbul.com.tr>
2026-09-12 17:37:07 +0300

    final5

--- python-module-03/ex5/ft_datastream.py
+++ python-module-03/ex5/ft_datastream.py
@@ -1,7 +1,8 @@
 from typing import Generator
 from random import choice
 
-def gen_event() -> Generator:
+
+def gen_event() -> Generator[tuple[str, str]]:
 
     actions = ["wake up", "sleep", "cry", "run", "walk", "kiss", "hug", "code"]
     names = ["Aleks", "Ilyas", "Ayse", "Ahmet", "Esra", "Zeliha"]
@@ -10,18 +11,21 @@ def gen_event() -> Generator:
         action = choice(actions)
         name = choice(names)
 
-        new_tuple = (name, action) 
+        new_tuple = (name, action)
         yield new_tuple
 
-def consume_event(event_list: list) -> Generator:
+
+def consume_event(
+    event_list: list[tuple[str, str]]
+) -> Generator[tuple[str, str], None, None]:
 
     while True:
         yield choice(event_list)
 
+
 def main() -> None:
     print("=== Game Data Stream Processor ===")
-    generator_gen_events = gen_event() #implemented the generator#
-    generator_get_event = consume_event()
+    generator_gen_events = gen_event()  # implemented the generator#
 
     for i in range(1000):
         events = next(generator_gen_events)
@@ -29,20 +33,21 @@ def main() -> None:
 
     list_for_events = []
     i = 0
-    while i < 10 :
+    while i < 10:
         events = next(generator_gen_events)
         list_for_events.append(events)
         i = i + 1
 
     print(f"Built list of 10 events : {list_for_events} ")
 
-    while i > 0 :
+    generator_get_event = consume_event(list_for_events)
+    while i > 0:
         events = next(generator_get_event)
-        print("Got event from list :",events)
+        print("Got event from list :", events)
         list_for_events.remove(events)
-        print("Remains in list :",list_for_events)
+        print("Remains in list :", list_for_events)
         i = i - 1
-    
-    
+
+
 if __name__ == "__main__":
-    main() 
+    main()
